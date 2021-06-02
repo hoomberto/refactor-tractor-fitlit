ca796a50df345dec3c947fc10fb27583ef37b9ea
Fara Akhatova <faraakhatova@gmail.com>
2021-06-02 17:56:51 -0600

    Remove commented pseudo

--- src/Hydration.js
+++ src/Hydration.js
@@ -7,10 +7,3 @@ class Hydration {
 }
 
 export default Hydration;
-
-// Create classes and methods that can calculate:
-//
-//  For a user (identified by their userID - this is the same for all methods requiring a specific user’s data), the average fluid ounces consumed per day for all time
-//  For a user, how many fluid ounces they consumed for a specific day (identified by a date)
-//  For a user, how many fluid ounces of water consumed each day over the course of a week (7 days) - return the amount for each day
-// You have to decide which classes should contain each method. Think about whose responsibility it is to own the method.
