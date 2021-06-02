f6aa9c9299330b3cfefd00ce3cd41c2ffad86eaf
Fara Akhatova <faraakhatova@gmail.com>
2021-06-02 17:56:14 -0600

    Remove extra sample data

--- test/Hydration-test.js
+++ test/Hydration-test.js
@@ -3,8 +3,6 @@ import {
 } from 'chai';
 
 import Hydration from '../src/Hydration';
-import UserRepository from '../src/UserRepository';
-import User from '../src/User';
 
 describe.only('Hydration', function() {
   let hydrate1;
@@ -16,12 +14,14 @@ describe.only('Hydration', function() {
       "date": "2019/06/15",
       "numOunces": 37
     });
+
     hydrate2 = new Hydration({
       "userID": 2,
       "date": "2019/06/15",
       "numOunces": 75
-    })
-  })
+    });
+  });
+
   it('should be a function', function() {
     expect(Hydration).to.be.a('function');
   });
@@ -32,7 +32,7 @@ describe.only('Hydration', function() {
     expect(hydrate2.userId).to.equal(2);
   });
   it('should have a date', function() {
-    expect(hydrate3.date).to.equal('2019/06/16');
+    expect(hydrate2.date).to.equal('2019/06/16');
   });
   it('should have an amount of ounces drank', function() {
     expect(hydrate3.ounces).to.equal(91);
