5ee2a7615432aec8d7fe0c8d12849cd6aa843ba8
StevenTheBearded <>
2021-06-03 18:43:25 -0400

    Sleep class method should calculate how many hours a user slept each day.

--- test/Sleep-test.js
+++ test/Sleep-test.js
@@ -1,11 +1,11 @@
 import { expect } from 'chai';
 
 import Sleep from '../src/Sleep';
-// import UserRepository from '../src/UserRepository';
 import User from '../src/User';
 
-describe.only('Sleep', function() {
-  let sleep1,sleep2,sleep3,user1,user2//,userRepository
+describe('Sleep', function() {
+  let sleep1, sleep2, sleep3, user1, user2
+
   beforeEach(() => {
     user1 = new User({
       'id': 1,
@@ -20,6 +20,7 @@ describe.only('Sleep', function() {
         8
       ]
     });
+
     user2 = new User({
       "id": 2,
       "name": "Jarvis Considine",
@@ -34,57 +35,75 @@ describe.only('Sleep', function() {
         19
       ]
     })
-    // userRepository = new UserRepository();
-    // userRepository.users.push(user1, user2);
+    
     sleep1 = new Sleep({
       "userID": 1,
       "date": "2019/06/15",
       "hoursSlept": 6.1,
       "sleepQuality": 2.2
-    });//, userRepository);
+    });
+    
     sleep2 = new Sleep({
       "userID": 2,
       "date": "2019/06/25",
       "hoursSlept": 7.3,
       "sleepQuality": 3.2
-    });//, userRepository);
-    sleep3 = new Sleep({
-      "userID": 1,
-      "date": "2019/07/17",
-      "hoursSlept": 9.3,
-      "sleepQuality": 1.4
-    });//, userRepository);
+    });
+
+    sleep3 = new Sleep ({
+      "userID":1,
+      "date":"2019/06/16",
+      "hoursSlept":4.1,
+      "sleepQuality":3.8
+    });
+    
   });
+
   it('should be a function', function() {
     expect(Sleep).to.be.a('function');
   });
+
   it('should be an instance of activity', function() {
     expect(sleep1).to.be.an.instanceof(Sleep);
   });
+
   it('should hold a userId', function() {
-    expect(sleep2.userId).to.equal(2);
+    expect(sleep2.userID).to.equal(2);
   });
+
   it('should hold a date', function() {
-    expect(sleep3.date).to.equal("2019/07/17");
+    expect(sleep2.date).to.equal("2019/06/25");
   });
+
   it('should hold hours slept', function() {
     expect(sleep1.hoursSlept).to.equal(6.1);
   });
+
   it('should hold sleep quality', function() {
-    expect(sleep3.sleepQuality).to.equal(1.4);
+    expect(sleep2.sleepQuality).to.equal(3.2);
   });
-  // describe('sleep', function() {
-    it('should update user\'s slept hours record', function() {
+
+  it.only('should calculate how many hours a user slept each day over the course of a week' , function () {
+    let sleepArr = [sleep1, sleep3]
+    
+    let totalHours = sleep1.getHoursSleptOverWeek(sleepArr)
+
+    expect(totalHours).to.equal(10.2)
+  });
+
+  it('should update user\'s slept hours record', function() {
       expect(user1.sleepHoursRecord.length).to.equal(2);
-    });
-    it('should update user\'s slept hours record', function() {
+  });
+
+  it('should update user\'s slept hours record', function() {
       expect(user2.sleepQualityRecord.length).to.equal(1);
-    });
-    it('should update user\'s slept hours average', function() {
+  });
+
+  it('should update user\'s slept hours average', function() {
       expect(user1.hoursSleptAverage).to.equal('7.7');
-    });
-    it('should update user\'s sleep quality average', function() {
+  });
+
+  it('should update user\'s sleep quality average', function() {
       expect(user1.sleepQualityAverage).to.equal('1.8');
-    });
-  // })
+  });
 });
