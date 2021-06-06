b0d07a5e9668e4179d0f82e2424169bf7f0af77f
Fara Akhatova <faraakhatova@gmail.com>
2021-06-06 13:59:13 -0600

    Add method for chart

--- src/charts/sleep-charts/allTime-sleep-chart.js
+++ src/charts/sleep-charts/allTime-sleep-chart.js
@@ -1,3 +1,13 @@
 export const renderAllTimeSleep = (currentUser, currentDate) => {
+  let hrsSlept = currentUser.hoursSleptAverageForAllDays(currentDate)
+  let sleepQuality = currentUser.sleepQualityAverageForAllDays(currentDate)
+
+  const sleepDataForAllTimes = {
+    labels: ['All Time Sleep Hours', 'All Time Sleep Quality'],
+    datasets: [{
+      data: [hrsSlept, sleepQuality],
+      backgroundColor: ['rgb(94,158,167)', 'rgb(158,214,183)']
+    }]
+  }
 
 }
